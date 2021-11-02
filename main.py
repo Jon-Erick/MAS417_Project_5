128500dcc1b57484ee4b4d99d7b2edee9ec57043
bsagh <test@hotmail.com>
2021-11-02 13:34:25 +0100

    upload images and grayscape them

--- main.py
+++ main.py
@@ -88,3 +88,29 @@ if "dayHigh": > "fiftyDayAverage":
  print('bear')
 '''
 
+####################################################
+# Image download and Grayscape
+#package needed
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+from stl import mesh
+
+#download BULL image, test to see it works
+im1 = Image.open("Chicago-Bulls-Emblem.jpg")
+#plt.imshow(im1)
+#plt.show()
+#download Bear image, test to see it works
+im2 = Image.open("Bear-Emblem.jpg")
+#plt.imshow(im2)
+#plt.show()
+
+#grayscale images
+gray_img1 = Image.open('Chicago-Bulls-Emblem.jpg').convert('LA')
+plt.imshow(gray_img1)
+plt.show()
+
+gray_img2 = Image.open('Bear-Emblem.jpg').convert('LA')
+plt.imshow(gray_img2)
+plt.show()
+
