00c5b8311477aa476ee8c2c5367ad6d04e6e5b91
bsagh <test@hotmail.com>
2021-11-09 11:36:12 +0100

    added grayscale and convert image in L band

--- STL_image.py
+++ STL_image.py
@@ -3,13 +3,23 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
-from stl import mesh
+#from stl import mesh
 
 #download BULL image, test to see it works
 im1 = Image.open("Chicago-Bulls-Emblem.jpg")
 #plt.imshow(im1)
 #plt.show()
+
 #download Bear image, test to see it works
 im2 = Image.open("Bear-Emblem.jpg")
 #plt.imshow(im2)
+#plt.show()
+
+#grayscale images
+gray_Bull = Image.open('Chicago-Bulls-Emblem.jpg').convert('L')
+#plt.imshow(gray_Bull)
+#plt.show()
+
+gray_Bear = Image.open('Bear-Emblem.jpg').convert('L')
+#plt.imshow(gray_Bear)
 #plt.show()
